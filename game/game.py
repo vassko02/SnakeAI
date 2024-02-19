15b1902ff4f24ae448833e3abc278615f9556285
vassko02 <vass.kornel@gmail.com>
2024-02-19 17:21:38 +0100

    játék manuálisan működik

--- game/game.py
+++ game/game.py
@@ -2,7 +2,9 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
+
 pygame.init()
+font=pygame.font.Font('arial.ttf',25)
 
 #irányok beállítása
 class Direction(Enum):
@@ -11,9 +13,19 @@ class Direction(Enum):
     UP=3
     DOWN=4
 
-Point=namedtuple('Point','x','y')
+Point=namedtuple('Point','x,y')
 
 BLOCK_SIZE=20
+SPEED=20
+
+GREEN=(125,255,50)
+RED=(200,0,0)
+RED2=(250,0,0)
+
+WHITE=(255,255,255)
+BLUE1=(0,100,255)
+BLUE2=(0,0,255)
+
 
 class SnakeGame:
     def __init__(self,w=640,h=480):
@@ -25,10 +37,10 @@ class SnakeGame:
         pygame.display.set_caption('SnakeAI')
         self.clock=pygame.time.Clock()
 
-        self.direction=Direction.UP
+        self.direction=Direction.RIGHT
 
         self.head=Point(self.w/2,self.h/2)
-        self.snake=[self.head,Point(self.head.x,self.head.y-BLOCK_SIZE),Point(self.head.x,self.head.y-(2*BLOCK_SIZE))]
+        self.snake=[self.head,Point(self.head.x-BLOCK_SIZE,self.head.y),Point(self.head.x-(2*BLOCK_SIZE),self.head.y)]
 
         self.score=0
         self.food=None
@@ -43,15 +55,99 @@ class SnakeGame:
             self._place_food()
 
 
-    def play_step(self):
-        pass
+    def play_step(self):  
+        #bemenet kezelése
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_LEFT:
+                    self.direction=Direction.LEFT
+                elif event.key==pygame.K_RIGHT:
+                    self.direction=Direction.RIGHT
+                elif event.key==pygame.K_UP:
+                    self.direction=Direction.UP
+                elif event.key==pygame.K_DOWN:
+                    self.direction=Direction.DOWN
+        
+        #mozgás
+        self._move(self.direction)
+        self.snake.insert(0,self.head)
+
+        #játék végének vizsgálata
+        game_over=False
+        if self._is_collision():
+            game_over=True
+            return game_over,self.score
+
+        #új étel vagy mozás
+        if self.head== self.food:
+            self.score+=1          
+            self._place_food()
+        else:
+            self.snake.pop()
+        #ui frissités
+        self._update_ui()
+        self.clock.tick(SPEED+self.score*3)
+
+        #játék vége
+        return game_over,self.score
+    
+    def _is_collision(self):
+        #falba ütközés
+        if self.head.x>self.w- BLOCK_SIZE or self.head.x<0 or self.head.y>self.h - BLOCK_SIZE or self.head.y < 0:
+            return True
+        #magába ütközés
+        if self.head in self.snake[1:]:
+            return True
+        return False
+
+    def _update_ui(self):
+        self.display.fill(GREEN)
+
+        #kígyó rajzolása      
+        idx=0
+        for pt in self.snake:
+            if idx==0:
+                pygame.draw.rect(self.display,BLUE2,pygame.Rect(pt.x,pt.y,BLOCK_SIZE,BLOCK_SIZE))
+                pygame.draw.rect(self.display,BLUE1,pygame.Rect(pt.x+4,pt.y+4,12,12))
+            else:
+                pygame.draw.rect(self.display,BLUE1,pygame.Rect(pt.x,pt.y,BLOCK_SIZE,BLOCK_SIZE))
+                pygame.draw.rect(self.display,BLUE2,pygame.Rect(pt.x+4,pt.y+4,12,12))
+            idx+=1
+
+        #étel rajzolása
+        pygame.draw.rect(self.display,RED,pygame.Rect(self.food.x,self.food.y,BLOCK_SIZE,BLOCK_SIZE))
+        pygame.draw.rect(self.display,RED2,pygame.Rect(self.food.x+4,self.food.y+4,12,12))
+
+        text=font.render("Score: "+str(self.score),True,WHITE)
+        self.display.blit(text,[0,0])
+        pygame.display.flip()
+
+
+    def _move(self,direction):
+        x=self.head.x
+        y=self.head.y
+        if direction==Direction.RIGHT:
+            x+=BLOCK_SIZE
+        elif direction==Direction.LEFT:
+            x-=BLOCK_SIZE
+        elif direction==Direction.UP:
+            y-=BLOCK_SIZE
+        elif direction==Direction.DOWN:
+            y+=BLOCK_SIZE
+        self.head= Point(x,y)
 
 if __name__=='__main__':
     game=SnakeGame()
 
     #játék loop
     while True:
-        game.play_step()
-
+        game_over,score=game.play_step()
+        if game_over==True:
+            break
+    
+    print('Final Score:', score)
 
     pygame.quit()
